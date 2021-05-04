ba5032a47e8912506471642caed7aead79fa55cd
akshay-vinod <akshayvinod468@gmail.com>
2021-05-04 17:02:40 +0530

    i think this will publish the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 import Search from "./components/Search";
-function App() {
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true);
+      const result = await axios(
+        `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${query}&date=04-05-2021`
+      );
+
+      console.log(result.data.sessions);
+
+      setData(result.data.sessions);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
   return (
     <div className="container">
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Search.css";
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState("");
 
-const Search = () => {
-  const [pincode, setPincode] = useState("");
-  const [enter, setEnter] = useState(false);
-  const [data, setData] = useState([]);
-  const [status, setStatus] = useState(true);
-  const [loading, setLoading] = useState(true);
-  const axios = require("axios");
-  const onChange = (e) => {
-    e.persist();
-    setPincode(e.target.value);
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
   };
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      const result = await axios(
-        `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=04-05-2021`
-      );
-      setData(result.data.sessions);
 
-      if (enter) {
-        setLoading(false);
-        if (data.length === 0) setStatus(false);
-      }
-    };
-    fetchData();
-    console.log(data);
-  }, [pincode, enter]);
   return (
     <div className="search-container">
       <div className="search">
@@ -36,35 +16,14 @@ const Search = () => {
             type="number"
             className="search-input"
             placeholder="Search.."
-            onChange={onChange}
+            value={text}
+            onChange={(e) => onChange(e.target.value)}
           />
 
-          <button className="search-button" onClick={() => setEnter(true)}>
+          <button className="search-button">
             <i className="fas fa-search" />
           </button>
         </div>
-      </div>
-      <div>
-        {loading ? (
-          <h1>loading...</h1>
-        ) : (
-          <h1>
-            {status
-              ? data.map(
-                  (items) => items.name + ":" + items.available_capacity + " "
-                )
-              : "no slot"}
-          </h1>
-          /*<section>
-            {enter ? (
-              data.map((items) => {
-                <h1>{items.name}</h1>;
-              })
-            ) : (
-              <h1>No Slot available</h1>
-            )}
-          </section>*/
-        )}
       </div>
     </div>
   );
